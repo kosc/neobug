df6721121c52da07ba7fbc480e32b479ce980cf0
Artyom K <hotkosc@gmail.com>
2014-08-14 02:18:46 +0400

    Debug mode for manage.py

--- manage.py
+++ manage.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python2
-from flask.ext.script import Manager
+from flask.ext.script import Manager, Server
 
 from neobug import neobug
 
 manager = Manager(neobug)
+manager.add_command("debug", Server(use_debugger=True, use_reloader=True))
 if __name__ == '__main__':
     manager.run()
