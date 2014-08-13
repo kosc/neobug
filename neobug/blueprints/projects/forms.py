e96a01cadc96a94ef88266e6d174d9d873796563
Artyom K <hotkosc@gmail.com>
2014-08-13 13:28:34 +0400

    Choose status and category when issue creating

--- neobug/blueprints/projects/forms.py
+++ neobug/blueprints/projects/forms.py
@@ -2,6 +2,11 @@ from flask.ext.mongoengine.wtf import model_form
 from wtforms.fields import *
 from neobug.models import Project, Issue, Comment
 
-IssueForm = model_form(Issue, exclude=['created_at', 'comments', 'author', 'status', 'category'])
+BaseIssueForm = model_form(Issue, exclude=['created_at', 'comments', 'author'])
 ProjectForm = model_form(Project, exclude=['created_at'])
 CommentForm = model_form(Comment, exclude=['created_at', 'author'])
+
+
+class IssueForm(BaseIssueForm):
+    status = SelectField(choices=zip(Issue.statuses, Issue.statuses))
+    category = SelectField(choices=zip(Issue.categories, Issue.categories))
--- neobug/forms.py
+++ neobug/forms.py
@@ -4,13 +4,6 @@ from neobug.models import *
 
 
 UserForm = model_form(User, exclude=['password_hash', 'password_salt'])
-ProjectForm = model_form(Project, exclude=['created_at', 'bugs'])
-IssueForm = model_form(Issue, exclude=['created_at',
-                                       'comments',
-                                       'author',
-                                       'status',
-                                       'category'])
-CommentForm = model_form(Comment, exclude=['created_at', 'author'])
 
 
 class RegisterForm(UserForm):
